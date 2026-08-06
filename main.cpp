95513b0d1724598137a0faa2448930593bca3978
jhai-pranesh-mcw <jhai.pranesh@multicorewareinc.com>
2026-08-06 10:38:16 +0530

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int number_1;
+    int number_2;
+    
+    cin>>number_1>>number_2;
+    
+    int sum;
+    
+    sum = number_1 + number_2;
+    
+    return 0;
+}

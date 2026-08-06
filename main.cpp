f1a1e4f6789c9505a35791fa5a5a08e64160c7f3
Jhai-pranesh <jhaipranesh@gmail.com>
2026-08-06 14:40:50 +0530

    ISSUE #2 Refactor : Refactored the code for modularity

--- main.cpp
+++ main.cpp
@@ -1,28 +1,59 @@
 #include<iostream>
 using namespace std;
+
+class Calculator
+{
+    public:
+        float number_1;
+        float number_2;
+    
+    Calculator(float num1, float num2)
+    {
+        number_1 = num1;
+        number_2 = num2;
+    }
+
+    float Addition()
+    {
+        return number_1 + number_2;
+    }
+
+    float Subraction()
+    {
+        return number_1 - number_2;
+    }
+
+    float Divide()
+    {
+        if(number_2 == 0)
+        {
+            cout<<"Division by zero is not possible";
+            return 0.0;
+        }
+        return number_1 / number_2;
+    }
+
+    int Remainder()
+    {
+        return (int)number_1 % (int)number_2;
+    }
+};
+
+
 int main()
 {
     //initialize the variables
-    int number_1;
-    int number_2;
+    float number_1;
+    float number_2;
     
     cin>>number_1>>number_2; //get the input
     
-    int sum;
-    
-    sum = number_1 + number_2;//calculate the sum
-    
-    int product;
+    Calculator calc(number_1, number_2);
 
-    product = number_1 * number_2;//calculate the product
-
-    int difference;
-
-    difference = number_1 - number_2;//calculate the difference
-
-    int quotient = 0;
-
-    if(number_2 != 0) quotient = number_1 / number_2;//calculate the quotient
+    cout<<"\n Total :" << calc.Addition();
+    cout<<"\n Subraction :" << calc.Subraction();
+    cout<<"\n Quotient :" << calc.Divide();
+    cout<<"\n Remainder :" << calc.Remainder();
     
     return 0;
 }
